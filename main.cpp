f35bb2da361c841db56c28e53ea3bd8807a4ceb4
ZiadElGafy <ziadelgafy9@gmail.com>
2021-05-15 12:15:18 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Network.hpp>
+using namespace std;
+
+void main()
+{
+	cout << "Hello World" << endl;
+
+	system("pause");
+}
