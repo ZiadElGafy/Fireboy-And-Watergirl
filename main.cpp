73c826e1d19df44bf0db55bbb241b2540e7505ff
ItsBeevo <Mesteeve.alaa.hamdy@gmail.com>
2021-05-16 21:44:48 +0200

    Back to normal

--- main.cpp
+++ main.cpp
@@ -41,6 +41,25 @@ using namespace sf;
 
 int main()
 {
+    RenderWindow window(VideoMode(1280, 720), "Fireboy and Watergirl");
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        window.display();
+    }
 
     return 0;
 }
