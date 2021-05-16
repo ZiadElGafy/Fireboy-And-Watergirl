7245ac1f9d8461f6bd556bc6477f6df6c25f004f
Hazem_Mohamed200 <71042723+Hazem-Mohamed200@users.noreply.github.com>
2021-05-16 17:18:03 +0200

    clion

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <SFML/Graphics.hpp>
-
+using namespace sf;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
 
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
