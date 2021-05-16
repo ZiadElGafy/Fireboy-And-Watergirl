28948ad58fdde9ccc89e14dd7a203af0fecb2bac
maramkhaled141 <maramkhaled141@gmail.com>
2021-05-16 16:54:17 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
-#include<iostream>
-#include<SFML/Graphics.hpp>
-#include<SFML/System.hpp>
-#include<SFML/Window.hpp>
-#include<SFML/Audio.hpp>
-#include<SFML/Network.hpp>
-using namespace std;
-using namespace sf;
+#include <SFML/Graphics.hpp>
+
 int main()
 {
-	system("pause");
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
