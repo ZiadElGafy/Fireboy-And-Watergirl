34aae723b0918b6732cdb6cdfeaa79435c5f9601
ItsBeevo <Mesteeve.alaa.hamdy@gmail.com>
2021-05-16 21:40:30 +0200

    Back to normal

--- main.cpp
+++ main.cpp
@@ -38,23 +38,26 @@
 
 using namespace std;
 using namespace sf;
+
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Blue);
+    RenderWindow window(VideoMode(1280, 720), "Fireboy and Watergirl");
 
     while (window.isOpen())
     {
         Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed) {
                 window.close();
+            }
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+                window.close();
+            }
         }
 
         window.clear();
-        window.draw(shape);
+
         window.display();
     }
 
